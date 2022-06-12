5ff66deb4ae36577be9b8d29f0b7931252829b1a
viettu2k <doanviettu2k@gmail.com>
2022-06-12 15:40:33 +0700

    hiding functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // import { User } from './User';
 // import { Company } from './Company';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
+const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
   zoom: 1,
   center: {
     lat: 0,
