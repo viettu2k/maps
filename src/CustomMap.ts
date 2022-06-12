87eec1ef35223f61f1a84b84691f127aef3f4942
viettu2k <doanviettu2k@gmail.com>
2022-06-12 16:18:34 +0700

    adding user maker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -13,4 +16,16 @@ export class CustomMap {
       }
     );
   }
+
+  addUserMaker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMaker(company: Company): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
+const user = new User();
 const customMap = new CustomMap('map');
+
+customMap.addUserMaker(user);
