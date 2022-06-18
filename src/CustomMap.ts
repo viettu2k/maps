924a92ac67121085c7b99c46b5bd02b428df16b0
viettu2k <doanviettu2k@gmail.com>
2022-06-18 21:50:19 +0700

    implict type check

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,3 @@
-import { User } from './User';
-import { Company } from './Company';
 // Instructions to every other class
 // on how they can be an argument to addMaker.
 interface Mappable {
@@ -34,14 +32,4 @@ export class CustomMap {
       },
     });
   }
-
-  // addCompanyMaker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
 }
